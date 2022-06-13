5e1f942870ed084cfbf246ee01b1107c7dcb19ea
Pate Bryant <patebry@gmail.com>
2022-06-13 18:08:53 -0400

    clean up header

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,14 @@ export default function Header({ name }) {
   return (
     <header className="p-2">
       <div className="flex items-center">
-        <div className="mr-2 w-20">
-          <Image src={pbLogo} alt="pb" className="dark:invert" />
-        </div>
+        <Image
+          src={pbLogo}
+          alt="pb"
+          className="dark:invert cursor-pointer"
+          width="80"
+          height="80"
+          onClick={() => (window.location.href = '/')}
+        />
       </div>
     </header>
   )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -37,11 +37,7 @@ export default function PostPage({
 }) {
   return (
     <Layout>
-      <SEO
-        title={`${frontMatter.title} - ${globalData.name}`}
-        description={frontMatter.description}
-      />
-      <Header name={globalData.name} />
+      <SEO title={frontMatter.title} description={frontMatter.description} />
       <article className="px-6 md:px-0">
         <header>
           <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
